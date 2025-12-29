a83ef61049707b27b334eec6bdba6a58d020606c
Tavilo Breno <breno_wk2@hotmail.com>
2025-12-29 02:32:12 -0300

    feat: Add remember_token, reset_token, and reset_expires_at to allowedFields in UserModel

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -69,6 +69,9 @@ class UserModel extends Model
         'password',
         'role',
         'active',
+        'remember_token',
+        'reset_token',
+        'reset_expires_at',
     ];
 
     // =========================================================================
